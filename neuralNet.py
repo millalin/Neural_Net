c071d5ddd5a4606abd5bda05dae2fab9740eda32
millalin <milla.lintunen@hotmail.com>
2020-02-23 14:24:14 +0200

    adding prediction of a model (same training and pred data) plots to show differences

--- neuralNet.py
+++ neuralNet.py
@@ -9,50 +9,40 @@ from keras.layers import Dense
 
 
 try:
-    user_data = pandas.read_csv("users.csv", delimiter = ";")
-    print("user data loaded succesfully")
+  user_data = pandas.read_csv("users.csv", delimiter = ";")
+  print("user data loaded succesfully")
 except:
-    print("error loading user data")
+  print("error loading user data")
 
 user_data.head()
 
 print(user_data.columns)
-
 
 # data with x values and target user count columns
 #all columns needed (how x affects L1547777 user count?)
 x_parameters_usercount_target_colums = [x for x in user_data 
 if x.endswith('_x') | x.endswith('L1547777')]
 
-
 x_parameters_columns= [x for x in user_data 
-if x.endswith('_x')]
+if x.endswith('our') | x.endswith('_x')]
 
 x_parameters = user_data[x_parameters_columns]
 
-
 # inputs
 inputs = np.array(user_data[x_parameters_columns])
-#inputs = user_data[x_parameters_columns]
 print("inputs")
 print(inputs)
 
 
 
-# other user counts
-user_counts_columns = [x for x in user_data if x.endswith('2212') | x.endswith('00545') | x.endswith('32034')
-| x.endswith('32035') | x.endswith('43298')| x.endswith('84705') | x.endswith('64453')
-| x.endswith('64454')]
-
 # user count cell for target cell
 user_counts_target_column = [x for x in user_data if x.endswith('L1547777')]
-
 
 
 # matrix for x variables and avg user count of target
 # here all needed values
 x_values = user_data[x_parameters_usercount_target_colums]
-print("koko taulukko" , x_values)
+print("whole matrix" , x_values)
 
 print(x_values.dtypes)
 
@@ -68,31 +58,29 @@ outputs = np.array(outputs)
 print("outputs now: ",outputs)
 
 
-
 plt.plot(x_values.L1547777)
 
 
-
-
-epochs = 600 # number of rounds (forward and backward)
+epochs = 1000 # number of rounds (forward and backward)
 batch_size = 500 # samples to put at once
 def define_model():
   model = Sequential()
-  model.add(Dense(10, input_shape=(9,), activation='relu'))
-  model.add(Dense(1))
+  model.add(Dense(64, input_shape=(10,), activation='relu')) #hidden layer and inputs size 10
+  model.add(Dense(1)) #output layer, output size 1
   print(model.summary())
   model.compile(loss='mse', optimizer='adam')
   return model
 
 def train_model(inp, outp, model, epochs, batch_size):
   h = model.fit(inp, outp, validation_split=0.2,
-               epochs=epochs,
-               batch_size=batch_size,
-               verbose=1)
-  plt.figure(figsize=(15,2.5))
+  epochs=epochs,
+  batch_size=batch_size,
+  verbose=1)
+  plt.figure(figsize=(15,2.6))
   plt.plot(h.history['loss'])
-  return model
+  plt.title(u"Loss during training", fontweight='bold', fontsize=20)
 
+  return model
 
 model = define_model()
 plt.figure(figsize=(10,3))
@@ -101,3 +89,25 @@ plt.title('Training data, avg user count, x parameter value')
 model = train_model(inputs, outputs, model, epochs, batch_size)
 
 plt.show()
+
+def predict_model():
+  pred = model.predict(inputs, batch_size=500)
+  
+  print(outputs,"pred", pred)
+
+
+  sns.distplot(outputs)
+  sns.distplot(pred)
+  plt.show()
+
+  plt.figure(figsize=(9,3))
+  plt.plot(pred, color="blue")
+  plt.plot(outputs, color="green")
+  plt.title(u"Actual outputs and predictions", fontweight='bold', fontsize=20)
+  plt.legend(['Predictions', 'Original user counts'])
+
+  plt.show()
+
+predict_model()
+
+
